39b8d18473b52d30ce8d8aa30622a6cdad0f4f92
SassCR <renefodao123@gmail.com>
2026-08-18 01:45:00 -0300

    correção de bugs

--- B1/aula2/ats/disciplinas.js
+++ B1/aula2/ats/disciplinas.js
@@ -1,67 +1,81 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-let produtos = [];
-let id = 1;
+let disciplinas = [];
+let id = 5;
 //get
-app.get("/produtos"
-, (req, res) => {
-res.json(produtos);
-});
-app.get("/produtos/:id"
-, (req, res) => {
-const produto = produtos.find(p => p.id ==
-req.params.id);
-if (!produto) {
-return res.status(404).json({ erro: "Produto não
-encontrado" });
-}
-res.json(produto);
-});
+app.get("/disciplinas"
+    , (req, res) => {
+        res.json(disciplinas);
+    });
+
+
+app.get("/disciplinas/:id"
+    , (req, res) => {
+        const disciplina = disciplinas.find(p => p.id ==
+            req.params.id);
+        if (!disciplina) {
+            return res.status(404).json({ erro: "Disciplina não encontrada" });
+        }
+        res.json(disciplina);
+    });
+
+
 //post
-app.post("/produtos"
-, (req, res) => {
-const { nome, preco } = req.body;
-if (!nome || !preco) {
-return res.status(400).json({ erro: "Dados
-inválidos" });
-}
-const novoProduto = {
-id: id++
-,
-nome,
-preco
-};
-produtos.push(novoProduto);
-res.status(201).json(novoProduto);
-});
+app.post("/disciplinas"
+    , (req, res) => {
+        const { nome, professor, horas } = req.body;
+        if (!nome || !professor || !horas) {
+            return res.status(400).json({ erro: "Dados inválidos" });
+        }
+        const novaDisciplina = {
+            id: id++
+            ,
+            nome,
+            professor,
+            horas
+        };
+        disciplinas.push(novaDisciplina);
+        res.status(201).json(novaDisciplina);
+    });
+
+
 //put
-app.put("/produtos/:id"
-, (req, res) => {
-const produto = produtos.find(p => p.id ==
-req.params.id);
-if (!produto) {
-return res.status(404).json({ erro: "Produto não
-encontrado" });
-}
-const { nome, preco } = req.body;
-if (nome) produto.nome = nome;
-if (preco) produto.preco = preco;
-res.json(produto);
-});
+app.put("/disciplinas/:id"
+    , (req, res) => {
+        const id = parseInt(req.params.id);
+        const { nome, professor, horas } = req.body;
+
+        const disciplina = disciplinas.find(d => d.id === id);
+
+        if (!disciplina) {
+            return res.status(404).send("Disciplina não encontrada!");
+        }
+
+        disciplina.nome = nome
+        disciplina.professor = professor
+        disciplina.horas = horas;
+        res.json({
+            mensagem: "Disciplina atualizada com sucesso!",
+            Disciplina: disciplina
+        })
+    });
+
+
 //delete
-app.delete("/produtos/:id"
-, (req, res) => {
-const index = produtos.findIndex(p => p.id ==
-req.params.id);
-if (index === -1) {
-return res.status(404).json({ erro: "Produto não
-encontrado" });
-}
-produtos.splice(index, 1);
-res.json({ mensagem: "Produto removido" });
-});
+app.delete("/disciplinas/:id"
+    , (req, res) => {
+        const index = disciplinas.findIndex(p => p.id ==
+            req.params.id);
+        if (index === -1) {
+            return res.status(404).json({ erro: "Disciplina não encontrada" });
+        }
+        disciplinas.splice(index, 1);
+        res.json({ mensagem: "Disciplina removida com sucesso!" });
+    });
+
+
 //porta pra subir o servidor
-app.listen(3000, () => {
-console.log("Servidor rodando na porta 3000");
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta 3003");
 });
